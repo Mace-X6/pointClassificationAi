59608740bee79605ef690d4ea7ff66de87db55e2
Mace_X6 <mees@vansteijn.info>
2023-02-21 21:16:17 +0100

    yay it works

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function generatePoints (amount, range){
 }
 
 // functionObj -> ax + by = 0 -> {a: int, b: int}, standard => a: 1, b: 1 -> y = x
-function classifyPoint(functionObj = { a : 1, b : 1 }, point){ 
+function classifyPoint (point, functionObj = { a : -1, b : 1 }){ 
     return point.x * functionObj.a + point.y * functionObj.b <= 0 ? 1 : -1;
 }
 
-function birthNewBrain(){
+function birthNewBrain (){
     var brain = {
         w1 : Math.random() * 2 - 1,
         w2 : Math.random() * 2 - 1
@@ -23,4 +23,37 @@ function birthNewBrain(){
     return brain;
 }
 
-console.log(birthNewBrain())
+function askAI (brain, point){
+    return point.x * brain.w1 + point.y * brain.w2 <= 0 ? 1 : -1;
+}
+
+function train (brain, point){
+    return error = (askAI(brain, point) - classifyPoint(point)) / 2;
+}
+
+function evolve (generations, learnAmount, brain = birthNewBrain()){
+    var points = generatePoints(generations, 100);
+
+    for (var i = 0; i < generations; i++){
+        var error = train(brain, points[i])
+        if (error != 0){
+            brain.w1 = brain.w1 + points[i].x * error * learnAmount
+            brain.w2 = brain.w2 + points[i].y * error * learnAmount
+        }
+    }
+    return brain;
+}
+
+function testBrain (brain, iterations){
+    var right = 0;
+    var pointArr = generatePoints(iterations, 100)
+    for (var i = 0; i < iterations; i++){
+        if (askAI(brain, pointArr[i]) === classifyPoint(pointArr[i])){
+            right++;
+        }
+    }
+    return ( right / iterations ) * 100 + '%'
+}
+
+var brain = evolve(10000, 0.1);
+console.log(testBrain(brain, 1000))

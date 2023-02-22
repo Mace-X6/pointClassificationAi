bde1d862aefee1630d36f8de21a6fa123403f13b
Mace-X6 <mees@vansteijn.info>
2023-02-22 11:05:24 +0100

    added a cool loading bar and variable functions

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ function generatePoints (amount, range){
     var pointArray = [];
     for (var i = 0; i < amount; i++){
         pointArray.push({
+            a : Math.random() * range,
             x : Math.random() * range,
+            b : Math.random() * range,
             y : Math.random() * range
         })
     }
@@ -11,35 +13,43 @@ function generatePoints (amount, range){
 }
 
 // functionObj -> ax + by = 0 -> {a: int, b: int}, standard => a: 1, b: 1 -> y = x
-function classifyPoint (point, functionObj = { a : -1, b : 1 }){ 
-    return point.x * functionObj.a + point.y * functionObj.b <= 0 ? 1 : -1;
+function classifyPoint (point){ 
+    return point.a * point.x + point.b * point.y <= 0 ? -1 : 1;
 }
 
 function birthNewBrain (){
     var brain = {
         w1 : Math.random() * 2 - 1,
-        w2 : Math.random() * 2 - 1
+        w2 : Math.random() * 2 - 1,
+        w3 : Math.random() * 2 - 1,
+        w4 : Math.random() * 2 - 1
     }
     return brain;
 }
 
 function askAI (brain, point){
-    return point.x * brain.w1 + point.y * brain.w2 <= 0 ? 1 : -1;
+    return point.x * brain.w1 + point.y * brain.w2 <= 0 ? -1 : 1;
 }
 
 function train (brain, point){
-    return error = (askAI(brain, point) - classifyPoint(point)) / 2;
+    return error = -(askAI(brain, point) - classifyPoint(point)) / 2;
 }
 
-function evolve (generations, learnAmount, brain = birthNewBrain()){
+function evolve (generations, learnRate, brain = birthNewBrain()){
+    console.clear()
+    var loaderSize = 30;
     var points = generatePoints(generations, 100);
 
     for (var i = 0; i < generations; i++){
         var error = train(brain, points[i])
         if (error != 0){
-            brain.w1 = brain.w1 + points[i].x * error * learnAmount
-            brain.w2 = brain.w2 + points[i].y * error * learnAmount
+            brain.w2 = brain.w2 + points[i].y * error * learnRate;
+            brain.w1 = brain.w1 + points[i].x * error * learnRate;
+            brain.w3 = brain.w3 + points[i].a * error * learnRate;
+            brain.w4 = brain.w4 + points[i].b * error * learnRate;
         }
+        console.clear();
+        console.log(`   [${'█'.repeat(Math.floor((i + 1) / generations * loaderSize)) + '_'.repeat(loaderSize - Math.floor((i + 1) / generations * loaderSize))}] ${Math.round(((i + 1) / generations * 10000)) / 100 + '%'}`);
     }
     return brain;
 }
@@ -52,9 +62,9 @@ function testBrain (brain, iterations){
             right++;
         }
     }
-    return ( right / iterations ) * 100 + '%'
+    return Math.round( ( right / iterations ) * 10000 ) / 100 + '%'
 }
 
-var brain = evolve(10000, 0.1);
+var brain = evolve(10**4, 0.01);
 console.log(brain)
-console.log(testBrain(brain, 1000))
+console.log(testBrain(brain, 10000))

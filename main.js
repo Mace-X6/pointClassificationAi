6c66cae13094fc7343e9a6635558a4ceceb9ea3a
Mace-X6 <mees@vansteijn.info>
2023-03-14 19:56:04 +0100

    cock
    oop

--- main.js
+++ main.js
@@ -1,55 +1,73 @@
 // the plot will be a square with sides of length range
-function generatePoints (amount, range){ 
+function generatePoints(amount, range) {
     var pointArray = [];
-    for (var i = 0; i < amount; i++){
+    for (var i = 0; i < amount; i++) {
         pointArray.push({
-            y : Math.random() * range,
-            x : Math.random() * range,
-            a : Math.random() * range
+            y: Math.random() * range,
+            x: Math.random() * range,
+            a: Math.random() * range
         })
     }
     return pointArray;
 }
 
 // functionObj -> ax + by = 0 -> {a: int, b: int}, standard => a: 1, b: 1 -> y = x
-function classifyPoint (point){ 
+function classifyPoint(point) {
     return point.b - point.y + point.x <= 0 ? -1 : 1;
 }
 
-function birthNewBrain (nodeArr){
+function birthNewBrain(nodeArr) {
     var nodeArr = nodeArr.concat([1]);
-    var brain = {w: [], b: [], accuracy: 0, nodeConfig: nodeArr};
-    for (var i = 0; i < nodeArr.length - 1; i ++){
+    var brain = {
+        w: [],
+        b: [],
+        accuracy: 0,
+        nodeConfig: nodeArr.slice(0)
+    };
+    for (var i = 0; i < nodeArr.length - 1; i++) {
         brain.w[i] = [];
-        for (var t = 0; t < nodeArr[i] * nodeArr[i + 1]; t ++){
-            brain.w[i].push(Math.random());
+        for (var t = 0; t < nodeArr[i] * nodeArr[i + 1]; t++) {
+            brain.w[i].push(1);
         }
     }
     nodeArr.shift();
-    for (var i = 0; i < nodeArr.length; i ++){
+    for (var i = 0; i < nodeArr.length; i++) {
         brain.b[i] = [];
-        for (var t = 0; t < nodeArr[i]; t ++){
-            brain.b[i].push(Math.random());
+        for (var t = 0; t < nodeArr[i]; t++) {
+            brain.b[i].push(1);
         }
     }
     return brain;
 }
 
-function askAI (brain, point){
-
-    true <= 0 ? -1 : 1;
+function askAI(brain, point) {
+    var input = [point.a, point.x, point.y];
+    var tempArr = [];
+    for (var i = 0; i < brain.nodeConfig.length; i++) {
+        i === 0 ? tempArr[0] = input : tempArr[i] = [];
+        if (i > 0) {
+            for (var t = 0; t < brain.nodeConfig[i]; t++) {
+                var tempNodeVal = 0;
+                for (var o = 0; o < tempArr[i - 1].length; o++) {
+                    tempNodeVal += tempArr[i - 1][o] * brain.w[i - 1][o + t];
+                }
+                tempArr[i].push(tempNodeVal);
+            }
+        }
+    }
+    return tempArr[tempArr.length - 1];
 }
 
-function train (brain, point){
+function train(brain, point) {
     return error = -(askAI(brain, point) - classifyPoint(point)) / 2;
 }
 
-function evolve (generations, learnRate, brain = birthNewBrain()){
+function evolve(generations, learnRate, brain = birthNewBrain()) {
     var points = generatePoints(generations, 100);
 
-    for (var i = 0; i < generations; i++){
+    for (var i = 0; i < generations; i++) {
         var error = train(brain, points[i])
-        if (error != 0){
+        if (error != 0) {
             brain.w2 = brain.w2 + points[i].y * error * learnRate;
             brain.w1 = brain.w1 + points[i].x * error * learnRate;
             brain.w3 = brain.w3 + points[i].a * error * learnRate;
@@ -59,15 +77,20 @@ function evolve (generations, learnRate, brain = birthNewBrain()){
     return brain;
 }
 
-function testBrain (brain, iterations){
+function testBrain(brain, iterations) {
     var right = 0;
     var pointArr = generatePoints(iterations, 100)
-    for (var i = 0; i < iterations; i++){
-        if (askAI(brain, pointArr[i]) === classifyPoint(pointArr[i])){
+    for (var i = 0; i < iterations; i++) {
+        if (askAI(brain, pointArr[i]) === classifyPoint(pointArr[i])) {
             right++;
         }
     }
-    return Math.round( ( right / iterations ) * 10000 ) / 100 + '%'
+    return Math.round((right / iterations) * 10000) / 100 + '%'
 }
 
-console.log(birthNewBrain([4,2,2]))
+console.log(askAI(birthNewBrain([3, 2, 2]), {
+    x: 1,
+    y: 1,
+    a: 1
+}))
+console.log(birthNewBrain([3, 2, 2, 2]))
